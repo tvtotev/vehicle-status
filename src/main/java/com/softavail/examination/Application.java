09c4baeb6b68625b2a1d90560a349638e90c5c3b
Totyo Totev <ttotev@axway.com>
2023-01-25 13:59:44 +0200

    Convert service to non-blocking

--- src/main/java/com/softavail/examination/Application.java
+++ src/main/java/com/softavail/examination/Application.java
@@ -2,8 +2,10 @@ package com.softavail.examination;
 
 import io.micronaut.runtime.Micronaut;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Application {
 
     public static void main(String... args) {
--- src/main/java/com/softavail/examination/VehicleStatusController.java
+++ src/main/java/com/softavail/examination/VehicleStatusController.java
@@ -1,5 +1,7 @@
 package com.softavail.examination;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.softavail.examination.model.VehicleStatus;
 import com.softavail.examination.model.VehicleStatusRequest;
+
+import io.micronaut.scheduling.annotation.Async;
 
 @RestController
 public class VehicleStatusController {
@@ -19,17 +24,22 @@ public class VehicleStatusController {
         this.vehicleStatusService = vehicleStatusService;
     }
 
+    @Async
     @GetMapping("/check")
-    public ResponseEntity<?>  check(@RequestParam(value = "vin", defaultValue = "") String vin) {
-        return ResponseEntity.ok(vehicleStatusService.check(vin));
+    public CompletableFuture<ResponseEntity<VehicleStatus>> check(
+            @RequestParam(value = "vin", defaultValue = "") String vin) {
+        return CompletableFuture.completedFuture(ResponseEntity.ok(vehicleStatusService.check(vin)));
     }
 
+    @Async
     @PostMapping("/check")
-    public ResponseEntity<?>  check(@RequestBody VehicleStatusRequest vehicleStatusRequest) {
+    public CompletableFuture<ResponseEntity<?>> check(@RequestBody VehicleStatusRequest vehicleStatusRequest) {
         try {
-            return ResponseEntity.ok(vehicleStatusService.check(vehicleStatusRequest.getVin(), vehicleStatusRequest.getFeatures()));
+            return CompletableFuture.completedFuture(ResponseEntity
+                    .ok(vehicleStatusService.check(vehicleStatusRequest.getVin(), vehicleStatusRequest.getFeatures())));
         } catch (ServiceUnavailableException e) {
-            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Service is temporary unavailable (CODE 503)\n");
+            return CompletableFuture.completedFuture(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body("Service is temporary unavailable (CODE 503)\n"));
         }
     }
 }
