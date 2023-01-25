c6a90e17aabf9c46f3f6bc89a65befb2976df209
Totyo Totev <ttotev@axway.com>
2023-01-25 13:35:01 +0200

    Tests: Adding test for scenario with no insurance nor maintenance services are requested

--- src/test/java/com/softavail/examination/VehicleStatusEndToEndTests.java
+++ src/test/java/com/softavail/examination/VehicleStatusEndToEndTests.java
@@ -7,6 +7,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -19,11 +20,14 @@ import java.util.Scanner;
 import java.util.Set;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Rule;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -46,9 +50,11 @@ import jakarta.inject.Inject;
 /**
  * Integration tests for the vehicle-checker service
  */
-@MicronautTest (rebuildContext = true)
-@Property(name = "endpoint.insurance", value = VehicleStatusEndToEndTests.SITE_SHEMA + "://" + VehicleStatusEndToEndTests.SITE)
-@Property(name = "endpoint.maintenance.frequency", value = "http://localhost")
+@MicronautTest(rebuildContext = true)
+@Property(name = "endpoint.insurance", value = VehicleStatusEndToEndTests.SITE_SHEMA + "://"
+        + VehicleStatusEndToEndTests.SITE)
+@Property(name = "endpoint.maintenance.frequency", value = VehicleStatusEndToEndTests.SITE_SHEMA + "://"
+        + VehicleStatusEndToEndTests.SITE)
 public class VehicleStatusEndToEndTests {
 
     @Inject
@@ -66,25 +72,27 @@ public class VehicleStatusEndToEndTests {
 
     @Property(name = "endpoint.maintenance.frequency")
     private String maintenanceFrequencyEndpoint;
-    
+
     private static int wireMockPort = 80;
 
     @Rule
-    public WireMockRule wireMockRule = new WireMockRule(wireMockPort);
-//    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort());
+    private static WireMockRule wireMockRule = new WireMockRule(wireMockPort);
     private static WireMockServer wireMockServer;
+//    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort());
 //    private WireMockServer wireMockServer = new WireMockServer();
     private CloseableHttpClient httpClient = HttpClients.createDefault();
-    
+
     private static Insurance insurance = null;
     private static String insuranceStr = null;
     private static MaintenanceFrequency maintenanceFrequency = null;
     private static String maintenanceFrequencyStr = null;
-    
-    
+
     @BeforeAll
-    public static void init() {
+    static void init() {
+        // Prepare response mocks for the external services
         ObjectMapper objectMapper = new ObjectMapper();
+
+        // Insurance mock
         InsuranceReport report = new InsuranceReport(2);
         insurance = new Insurance(report);
         try {
@@ -92,17 +100,15 @@ public class VehicleStatusEndToEndTests {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
+
+        // Maintenance mock
         maintenanceFrequency = new MaintenanceFrequency(MaintenanceFrequencyType.verylow.toString());
         try {
             maintenanceFrequencyStr = objectMapper.writeValueAsString(maintenanceFrequency);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-    }
-
-    
-    @Test
-    public void givenProgrammaticallyManagedServer_whenUsingSimpleStubbing_thenCorrect() throws IOException {
+        // Start WireMock
         if (!wireMockRule.isRunning()) {
             wireMockRule.start();
         }
@@ -114,36 +120,51 @@ public class VehicleStatusEndToEndTests {
             } catch (Exception e) {
             }
         }
-        
+
+    }
+
+    @AfterAll
+    static void shutdown() {
+        wireMockRule.stop();
+        wireMockServer.stop();
+    }
+
+    @BeforeEach
+    void beforeEach() throws ClientProtocolException, IOException {
+        // Mock responses: Insurance and Maintenance external services
         configureFor(SITE_SHEMA, SITE, wireMockPort);
         stubFor(get(urlEqualTo(INSURANCE_PATH))
                 .willReturn(aResponse().withBody(insuranceStr).withHeader("Content-Type", "application/json")));
         stubFor(get(urlEqualTo(FREQ_MAINT_PATH)).willReturn(
-                aResponse().withBody(maintenanceFrequencyStr).withHeader("Content-Type", "application/json")));        
+                aResponse().withBody(maintenanceFrequencyStr).withHeader("Content-Type", "application/json")));
 
         HttpGet request;
         HttpResponse httpResponse;
         String stringResponse;
-        
-        request= new HttpGet(SITE_SHEMA + "://" + SITE + ":" + wireMockPort + INSURANCE_PATH);
+
+        // Check whether Insurance external service is mocked
+        request = new HttpGet(SITE_SHEMA + "://" + SITE + ":" + wireMockPort + INSURANCE_PATH);
         httpResponse = httpClient.execute(request);
         stringResponse = convertResponseToString(httpResponse);
         verify(getRequestedFor(urlEqualTo(INSURANCE_PATH)));
         assertEquals(insuranceStr, stringResponse);
 
-        request= new HttpGet(SITE_SHEMA + "://" + SITE + ":" + wireMockPort + FREQ_MAINT_PATH);
+        // Check whether Maintenance external service is mocked
+        request = new HttpGet(SITE_SHEMA + "://" + SITE + ":" + wireMockPort + FREQ_MAINT_PATH);
         httpResponse = httpClient.execute(request);
         stringResponse = convertResponseToString(httpResponse);
         verify(getRequestedFor(urlEqualTo(FREQ_MAINT_PATH)));
         assertEquals(maintenanceFrequencyStr, stringResponse);
 
-        postWithRequestBodyAndPostMappingWorks(VIN);
-        
-        wireMockServer.stop();
     }
-    
-    
 
+    /**
+     * Converts response to String
+     * 
+     * @param response
+     * @return Text of the response
+     * @throws IOException
+     */
     private static String convertResponseToString(HttpResponse response) throws IOException {
         InputStream responseStream = response.getEntity().getContent();
         Scanner scanner = new Scanner(responseStream, "UTF-8");
@@ -151,19 +172,38 @@ public class VehicleStatusEndToEndTests {
         scanner.close();
         return stringResponse;
     }
-    
-    void postWithRequestBodyAndPostMappingWorks(String vin) {
+
+    /**
+     * Integration test for positive scenario
+     */
+    @Test
+    void endToEndPositiveTestWithRequestingInsuranceAndMaintenanceServices() throws IOException {
+        // Perform end-to-end test
         Set<String> features = Collections.unmodifiableSet(
                 new HashSet<>(Arrays.asList(Feature.ACCIDENT_FREE.toString(), Feature.MAINTANANCE.toString())));
-
-        VehicleStatusRequest request = new VehicleStatusRequest(vin, features);
+        VehicleStatusRequest request = new VehicleStatusRequest(VIN, features);
         VehicleStatus response = vehicleStatusClient.check(request);
         assertNotNull(response);
         assertNotNull(request.getFeatures());
         assertEquals(request.getVin(), response.getVin());
         assertEquals(MaintenanceScore.POOR, response.getMaintenanceScores());
         assertEquals(Boolean.FALSE, response.isAccidentFree());
-        
+    }
+
+    /**
+     * Integration test for negative scenario. Neither insurance you nor maintenance
+     * is requested
+     */
+    @Test
+    void postWithRequestBodyAndEmtyServiceList() {
+        Set<String> features = Collections.unmodifiableSet(new HashSet<>(Arrays.asList()));
+        VehicleStatusRequest request = new VehicleStatusRequest(VIN, features);
+        VehicleStatus response = vehicleStatusClient.check(request);
+        assertNotNull(response);
+        assertEquals(0, request.getFeatures().size());
+        assertEquals(request.getVin(), response.getVin());
+        assertNull(response.getMaintenanceScores());
+        assertNull(response.isAccidentFree());
     }
 
 }
